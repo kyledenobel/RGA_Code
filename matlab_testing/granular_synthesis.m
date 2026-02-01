e1a426edb3932a88cd3814f902a07a1eb20965ca
de Nobel, Kyle James <knobel3@gatech.edu>
2026-01-31 19:21:15 -0500

    added folder for matlab testing of digital effects. Currently there is a script for granular synthesis (pitch shifting)

---
+++ matlab_testing/granular_synthesis.m
@@ -0,0 +1,84 @@
+% Attempt at granular synthesis for pitch shifting
+rho = 1;
+
+% ============= begin implementation =============  %
+
+% read audio sample
+[x, Fs] = audioread("G-scale.wav");
+samples = 6*Fs; % 3 seconds of samples
+x = x(1:samples, :);
+
+%x = sin(200*2*pi*(1:(1/Fs):5));
+
+% let S = the stride length  (grain period = 10ms)
+T = 0.02;
+S = floor(Fs*T);
+
+% generate output
+y = zeros(numel(x),1);
+
+gamma = 1/(1+(rho*S)/(S-1));
+
+number_of_times_buffer_filled = 0;
+
+for n = 0:(numel(x))-1
+    % only fill buffers when we need to
+    if mod(n, S) == 0
+        % generate buffers
+        a = zeros(1, 2*S);
+        b = zeros(1, 2*S);
+        % fill buffers
+        for k = 0:((rho+1)*S)-1
+            if floor(k*gamma + 1 + (S*number_of_times_buffer_filled)) < numel(x) + 1
+                a(k+1) = x(floor(k*gamma + 1 + (S*number_of_times_buffer_filled)));
+            else
+                a(k+1) = 0;
+            end
+            if floor(k*gamma + 1 + S + (S*number_of_times_buffer_filled)) < numel(x) + 1
+                b(k+1) = x(floor(k*gamma + 1 + S + (S*number_of_times_buffer_filled)));
+            else
+                b(k+1) = 0;
+            end
+        end
+        number_of_times_buffer_filled = number_of_times_buffer_filled + 1;
+    end
+
+    % calculate window
+    m = mod(n,S);
+    if rho == 0
+        w = 1;
+    else
+        w = (((m/(S*rho)) * m) < (S*rho)) + (1 * (m >= (S*rho)));
+    end
+
+    % do computation of y
+    y(n+1) = w*(b(mod(n, S) + 1)) + (1 - w)*(a(S + mod(n, S) + 1));
+end
+
+
+% play output
+
+player = audioplayer(y, Fs);
+play(player)
+
+figure(1)
+subplot(2,1,1)
+plot(x)
+subplot(2,1,2)
+plot(y)
+
+
+X = fft(x, int32(2*Fs));
+X = fftshift(X);
+
+Y = fft(y, int32(2*Fs));
+Y = fftshift(Y);
+
+x_axis = linspace(-Fs, Fs, int32(2*Fs))./2;
+figure(2)
+subplot(2,1,1)
+plot(x_axis, abs(X))
+xlim([-500, 500])
+subplot(2,1,2)
+plot(x_axis, abs(Y))
+xlim([-500, 500])
