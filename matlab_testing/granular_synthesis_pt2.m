c724afb983d744ebaf53181d7413603ad5092fba
de Nobel, Kyle James <knobel3@gatech.edu>
2026-02-13 23:45:44 -0500

    modifications to granular synthesis algorithm

--- matlab_testing/granular_synthesis_pt2.m
+++ matlab_testing/granular_synthesis_pt2.m
@@ -3,7 +3,7 @@
 % 1) Interpolation between values in buffers
 % 2) 
 
-rho = 0.3;
+rho = 0.7;
 
 
 % ========== begin implementation ========== %
@@ -11,6 +11,11 @@ rho = 0.3;
 [x, Fs] = audioread("G-scale.wav");
 samples = 6*Fs; % 3 seconds of samples
 x = x(1:samples, :);
+
+Fs = 10000;
+tt = 0:1/Fs:2;
+f = 100;
+x = cos(2*pi*tt*f);
 
 % let S = the stride length  (grain period = 25ms)
 T = 0.025;
@@ -22,9 +27,24 @@ y = zeros(numel(x),1);
 % parameters
 gamma = 1/(1+rho);
 
-a = zeros(1, 2*S);
-b = zeros(1, 2*S);
-offset = rand()*30 - 15;
+% initialize buffers
+a = zeros(1, floor(S/gamma));
+b = zeros(1, floor(S/gamma));
+c = zeros(1, floor(S/gamma));
+w = zeros(1, floor(S/gamma));
+
+% calculate window
+for k = 1:floor(S/gamma)
+    if k < rho*S
+        w(k) = 1/(rho*S)*k;
+    elseif k <= S
+        w(k) = 1;
+    else
+        w(k) = (1/(S-(S/gamma)))*k - 1/(gamma-1);
+    end
+end
+
+
 
 for i = 1:numel(x)
 
@@ -41,17 +61,6 @@ for i = 1:numel(x)
                 b(k) = (1-(k*gamma-floor(k*gamma)))*x(floor(k*gamma) + i + S) + (k*gamma-floor(k*gamma))*x(ceil(k*gamma) + i + S);
             end
         end
-        offset = rand()*30 - 15;
-    end
-
-    % stitch buffers together
-    % calculate window
-    m = mod(i,S);
-    if rho == 0
-        w = 1;
-    else
-        %w = myLinearWindow(m, S, rho, offset);
-        w = mySineWindow(m, S, rho, 0);
     end
 
     % do computation of y
@@ -73,6 +82,7 @@ plot(y);
 % plot just one note
 i = 76615:95063;
 i = 76615:80000;
+i = 1:600;
 xx = x(i);
 yy = y(i);
 figure(2)
@@ -97,23 +107,5 @@ subplot(2,1,2)
 plot(x_axis, abs(Y))
 xlim([-2000, 2000])
 
-
-
-
-function v = myLinearWindow(m, S, rho, offset)
-    if m < (S*rho + offset)
-        v = m/(S*rho);
-    else
-        v = 1;
-    end
-end
-
-
-function v = mySineWindow(m, S, rho, offset)
-    v = sin(m*2*pi*(1/(2*(S*(1+rho) + offset))));
-    if v < 0
-        v = 0;
-    end
-end
 
 
